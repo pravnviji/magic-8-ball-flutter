fcf3e6bd381746ed6d4f9affe5204fa01f34eecc
pravnviji <praveenbabu.it2009@gmail.com>
2020-07-19 08:48:15 +0800

    Added Magic ball functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0XFF0D47A1),
+        title: Text('Ask me anything'),
+      ),
+      body: (Container(
+        color: Colors.blue,
+        child: Ball(),
+      )),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _Ball createState() => _Ball();
+}
+
+class _Ball extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(4) + 1;
+            print(ballNumber);
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
